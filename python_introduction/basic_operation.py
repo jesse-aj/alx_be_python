c6d56e546b7eff3c01807060f5178c57b7f2f0de
jesse-aj <appiahjesse123@gmail.com>
2025-11-11 13:51:04 +0000

    Created a simple calculator

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 15
+sum = number1 + number2
+
+difference  = number2 - number1
+
+product = number1 * number2
+
+
+print(f"\n Addition of {number1} and {number2} is {sum}")
+
+ 
+print(f"\n Subtraction of {number2} and {number1} is {difference}")
+
+print(f"\n Multiplication of {number1} and {number2} is {product}")
+
