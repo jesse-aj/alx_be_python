3ea96518bd2498d191e921b46851a7332ee1ac77
jesse-aj <appiahjesse123@gmail.com>
2025-11-26 19:44:20 +0000

    created a simple daily remainder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,23 @@
+task = input("Enter your task: ").strip().lower()
+priority = input("Priority (high, medium, low) ").strip().lower()
+time_bound = input("Is it time-bound? (yes/no) ").strip().lower()
+
+match priority:
+    case "high":
+        print(f"Reminder: {task} is a high priority task", end="")
+
+    case "low":
+        print(f"Reminder : {task} is a low priority task", end="")
+
+    case "medium":
+        print(f"Reminder : {task} is a medium priority task", end="")
+    case _:
+        print("Invalid priority")
+
+
+if time_bound == "yes":
+    print(f" that requires immediate attention today!", end="")
+else:
+    print(f".Consider completing it when you have free time", end="")
+
+print()
