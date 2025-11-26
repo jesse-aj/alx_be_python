d2bfdcd7eee97590492e2d6a84f173a70684fca2
jesse-aj <appiahjesse123@gmail.com>
2025-11-26 00:44:18 +0000

    fixed error to match auto checker

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,5 @@
 weather = input(
-    "What's the weather like today? (sunny/rainy/cold): ").strip().lower()
+    "What's the weather like today? (sunny/rainy/cold):").strip().lower()
 if weather == 'sunny':
     print('Wear a t-shirt and sunglasses')
 elif weather == "rainy":
