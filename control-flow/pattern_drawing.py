5384dd1270b3381233fd19c55f033ca17a8bafba
jesse-aj <appiahjesse123@gmail.com>
2025-12-05 22:58:30 +0000

    Optimized code for better UI

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,7 @@
 size = int(input("Enter the size of the pattern:"))
 i = 0
 while i < size:
+    # This checks range in size
     for j in range(size):
         print("*", end="")
     print()
--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,16 +1,13 @@
-number1 = 10
-number2 = 5
+number1 = int(input("Please enter your first number: "))
+number2 = int(input("Please enter your second number: "))
+
 sum = number1 + number2
-
-difference  = number2 - number1
-
+difference = number2 - number1
 product = number1 * number2
 
 
 print(f"\n Addition of {number1} and {number2} is {sum}")
 
- 
 print(f"\n Subtraction of {number2} and {number1} is {difference}")
 
 print(f"\n Multiplication of {number1} and {number2} is {product}")
-
