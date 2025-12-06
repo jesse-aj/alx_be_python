0dce1374bdbb0280972e7e430ecb49e62d977918
jesse-aj <appiahjesse123@gmail.com>
2025-12-06 23:06:35 +0000

    Created a script  that implements a simple interface for managing a shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add = input("Please enter the item name: ")
+            shopping_list.append(add)
+            pass
+            
+        elif choice == '2':
+            remove = input("Please Enter item you want to remove: ")
+            shopping_list.remove(remove)
+            pass
+
+        elif choice == '3':
+            print(*shopping_list, sep=", ")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+    
+
+      
+if __name__ == "__main__":
+    main()
