55758e22d5acec98cde22d3e15b056940ed160b6
jesse-aj <appiahjesse123@gmail.com>
2025-12-07 00:30:25 +0000

    fixed spaces in date time conversion

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    print(current_date.strftime("%Y-%m-%d  %H:%M:%S"))
+    print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
     return current_date
 
 
