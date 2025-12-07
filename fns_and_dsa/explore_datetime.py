bdf781b3657fca5d47409070c5a4587bdacd13b3
jesse-aj <appiahjesse123@gmail.com>
2025-12-07 00:28:43 +0000

    corrected function name from curent to current

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 
-def display_curent_datetime():
+def display_current_datetime():
     current_date = datetime.now()
     print(current_date.strftime("%Y-%m-%d  %H:%M:%S"))
     return current_date
@@ -9,10 +9,10 @@ def display_curent_datetime():
 
 def calculate_future_date(current_date):
     num_days = int(input("Enter the number of days: "))
-    future_date = current_date + timedelta(num_days)
+    future_date = current_date + timedelta(days=num_days)
     print(future_date.strftime("%Y-%m-%d"))
     return future_date
 
 
-current_date = display_curent_datetime()
+current_date = display_current_datetime()
 calculate_future_date(current_date)
