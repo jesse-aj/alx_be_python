5c45cee3691cf73b15d9b96627595faac1161881
jesse-aj <appiahjesse123@gmail.com>
2025-12-31 14:14:05 +0000

    Implemented dunder methods under each class

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,17 +3,26 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size: int):
         self.file_size = file_size
         super().__init__(title, author)
 
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count: int):
         self.page_count = page_count
         super().__init__(title, author)
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
@@ -25,10 +34,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(
-                    f"Ebook: {book.title} by {book.author} File Size:{book.file_size}")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author} Page Count:{book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
