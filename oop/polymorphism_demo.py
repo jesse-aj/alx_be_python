ff5fbae5018201946a4f753431b2e601d9ed0010
jesse-aj <appiahjesse123@gmail.com>
2025-12-31 14:44:28 +0000

    Exploring Polymorphism and Method Overriding

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError
+    
+class Rectangle(Shape):
+    def __init__(self, lenght, width):
+        self.lenght = lenght
+        self.width = width
+
+    def area(self):
+        results = self.lenght * self.width
+        return results
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+       
+
+    def area(self):
+        results = math.pi * self.radius
+        return results
+
+
